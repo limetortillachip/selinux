acadeb769c2ca16ad9cdacfab462a4eb397ea9fc
limetortillaship <shanonps@icloud.com>
2026-01-02 14:53:18 -0500

    style page body with flex-grid

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,31 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
-  import Footer from '../components/Footer/Footer.svelte';
-  import Header from '../components/Header/Header.svelte';
+  import favicon from "$lib/assets/favicon.svg";
+  import Footer from "../components/Footer/Footer.svelte";
+  import Header from "../components/Header/Header.svelte";
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+  <link rel="icon" href={favicon} />
 </svelte:head>
 
-<Header />
-{@render children()}
-<Footer />
+<div class="page">
+  <Header />
+  {@render children()}
+  <Footer />
+</div>
+
+<style>
+  /* normalize for reseting elements */
+  @import url("https://necolas.github.io/normalize.css/8.0.1/normalize.css");
+
+  /* page class w/ flex */
+  .page {
+    display: inline-flex;
+    height: 100vh;
+    width: 100%;
+    background-color: red;
+    flex-flow: column nowrap;
+  }
+</style>
