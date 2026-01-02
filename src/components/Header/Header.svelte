45b47126cd71f1943510bf43bf769f75a8b7a0b4
limetortillaship <shanonps@icloud.com>
2026-01-02 14:15:36 -0500

    Create components folder and Header component for main page

---
+++ src/components/Header/Header.svelte
@@ -0,0 +1,21 @@
+<div>
+    <header>
+        <h1>selinux</h1>
+    </header>
+</div>
+<div>
+    <nav>
+        <ul>
+            <li>
+                <a href="/" aria-label="home-link">home</a></li>
+            <li>
+                <a href="/about" aria-label="about-me-page-link">about me</a></li>
+            <li>
+                <a href="photos" aria-label="photos-page-link">photos</a></li>
+            <li>
+                <a href="/blog" aria-label="blog-link">blog</a></li>
+            <li>
+                <a href="/credits" aria-label="credits-page-link">credits</a></li>
+        </ul>
+    </nav>
+</div>
