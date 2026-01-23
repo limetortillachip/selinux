97bf805b921442dcf56c8438745f5690a88e1736
limetortillaship <shanonps@icloud.com>
2026-01-23 13:00:08 -0500

    Added filter buttons for photos page

--- src/routes/photos/+layout.svelte
+++ src/routes/photos/+layout.svelte
@@ -2,16 +2,44 @@
   let { children } = $props();
 </script>
 
-<div class="photos-page">
+<div class="photos-page grid">
+  <div class="photos-page-header">
   <h2>Photos here</h2>
+  <div class="filters">
+    <span>
+      <h3 id="filters-title">Filters</h3>
+    </span>
+    <ul class="filters-links grid">
+      <li id="nyc">New York City</li>
+      <li id="fall">Fall</li>
+      <li id="shotoniphone">#shotoniPhone</li>
+    </ul>
+</div>
+</div>
   {@render children()}
 </div>
 
 <style>
+  .grid {
+    display: flex;
+}
+
   .photos-page {
     flex: 90% 0;
     margin: 0 auto;
-    display: flex;
     flex-flow: column nowrap;
   }
+
+  .filters-links {
+    padding-inline-start: 0;
+    gap: 2em;
+    list-style-type: none;
+  }
+
+  .filters-links>li {
+    background-color: red;
+    padding: 0.25em 0.5em;
+  }
+
+
 </style>
